083081141f323c2c361072059ea24feb552fef36
that-damn-cat <saihenjin@gmail.com>
2025-12-01 10:44:18 -0600

    Updated to most recent version from ComponentPlayground

--- Components/State/State.gd
+++ Components/State/State.gd
@@ -1,32 +1,49 @@
+## A node based State for the StateMachine
+
 @icon("./choice.svg")
 class_name State
 extends Node
 
+## Emits when this state is requesting a transition to a new state.
 signal transitioned(this_state: State, new_state_name: String)
-var state_machine : StateMachine
 
+## The state name used to find this state in the StateMachine
 @export var state_name: String = name.to_lower()
+
+## Holds a reference to the StateMachine that controls this State
+var state_machine: StateMachine
+
 
 func _ready() -> void:
 	if get_parent() is not StateMachine:
-		push_error("State nodes must be children of a StateMachine node.")
+		push_error("State nodes must be children of a StateMachine Node!")
 
 
+## To be implemented by the inheriting node. Called when the state is first entered.
 func enter() -> void:
 	pass
 
 
+## To be implemented by the inheriting node. Called when the state is exited.
 func exit() -> void:
 	pass
 
 
+## To be implemented by the inheriting node. Called with _process
 func update(_delta: float) -> void:
 	pass
 
 
+## To be implemented by the inheriting node. Called with _physics_process
 func physics_update(_delta: float) -> void:
 	pass
 
 
+## Emit the transitioned signal
 func transition_to(new_state_name: String) -> void:
 	transitioned.emit(self, new_state_name)
+
+
+func _exit_tree() -> void:
+	if state_machine:
+		state_machine.remove_state(self)
--- Components/StateMachine/StateMachine.gd
+++ Components/StateMachine/StateMachine.gd
@@ -1,11 +1,21 @@
+## A Node based state machine that manages State children and handles transitions
 @icon("./gears.svg")
 class_name StateMachine
 extends Node
 
+## Emitted when a state transition occurs
 signal state_changed(old: State, new: State)
+
+## The state set at _ready
 @export var initial_state: State
+
+## Dictionary of currently configured states
 var states: Dictionary[StringName, State] = { }
+
+## Current state
 var current_state: State
+
+## Last running state
 var previous_state: State
 
 
@@ -21,27 +31,8 @@ func _ready() -> void:
 	if initial_state:
 		_set_state(initial_state)
 
-	var state_children: Array[State] = []
-	for child in get_children():
-		if child is State:
-			state_children.append(child)
-
-	if state_children.size() == 0:
+	if states.size() == 0:
 		push_warning("StateMachine has no State children at ready!")
-
-	var state_names := {}
-	var duplicates := ""
-
-	for state in state_children:
-		var this_name := state.state_name
-
-		if state_names.has(this_name):
-			duplicates += "- '%s'\n" % this_name
-		else:
-			state_names[this_name] = true
-
-	if not duplicates == "":
-		push_error("Duplicate state_name values detected:\n" + duplicates)
 
 
 func _process(delta: float) -> void:
@@ -55,31 +46,33 @@ func _physics_process(delta: float) -> void:
 
 
 ## Returns the state object with the given name
-func get_state(state_name : String) -> State:
+func get_state(state_name: String) -> State:
 	return(states.get(state_name))
 
 
 ## Adds a given state to the State Machine
-func add_state(new_state : State) -> void:
+func add_state(new_state: State) -> void:
 	new_state.state_machine = self
 
-	# The new state must be a direct child of the StateMachine.
-	if new_state.get_parent() != self:
-		new_state.get_parent().remove_child(new_state)
-		add_child(new_state)
+	# The new state must be a direct child of the StateMachine. Put it there if it isn't.
+	if not new_state.get_parent() == self:
+		# Defer calls to end of frame in case reparenting is already happening from somewhere else
+		new_state.get_parent().call_deferred("remove_child", new_state)
+		self.call_deferred("add_child", new_state)
 
 	if states.has(new_state.state_name):
-		push_warning("Duplicate state_name '%s' detected." % new_state.state_name)
+		push_warning("Duplicate state_name '%s' detected. State being overwritten!" % new_state.state_name)
+		remove_state(new_state)
 
 	states[new_state.state_name] = new_state
 	new_state.transitioned.connect(_on_state_transitioned)
 
 ## Removes a given state from the State Machine, leaving it in the tree.
-## If the removed state is the current state, transitions to the previous state.
-## If there is no previous state, transitions to the initial state.
-## Finally, if there is no initial state, the StateMachine will have no current state.
+## If the removed state is the current state, becomes stateless.
+## If the removed state is initial or previous, null those values.
 func remove_state(state_to_remove: State) -> void:
 	if not states.has(state_to_remove.state_name):
+		push_warning("Error trying to remove state '%s', state not in dictionary!" % state_to_remove.state_name)
 		return
 
 	# Remove references to the removed state in previous/initial
@@ -89,15 +82,12 @@ func remove_state(state_to_remove: State) -> void:
 	if state_to_remove == initial_state:
 		initial_state = null
 
-	# If we are removing the current state, try to handle it gracefully.
+	# If we are removing the current state, become stateless
 	if state_to_remove == current_state:
-		if previous_state:
-			change_state(previous_state.state_name)
-		elif initial_state:
-			change_state(initial_state.state_name)
-		else:
-			current_state = null
+		current_state.exit()
+		current_state = null
 
+	# Remove it from the dictionary and disconnect its transition signal
 	states.erase(state_to_remove.state_name)
 	state_to_remove.transitioned.disconnect(_on_state_transitioned)
 
@@ -109,7 +99,7 @@ func has_state(state_name: String) -> bool:
 
 ## Forces a state change to the given state name.
 func change_state(new_state_name: String) -> void:
-	var new_state := get_state(new_state_name)
+	var new_state: State = get_state(new_state_name)
 
 	if not new_state:
 		push_error("State '%s' does not exist in the state machine." % new_state_name)
@@ -120,9 +110,7 @@ func change_state(new_state_name: String) -> void:
 
 func _on_state_transitioned(this_state: State, new_state_name: String) -> void:
 	if this_state != current_state:
-		push_error("State transition signal received from a state that is not the current state.")
-		push_error("Current State: %s" % current_state)
-		push_error("Signal received from: %s" % this_state)
+		push_warning("State transition signal received from a state '%s' which is not the current state!" % this_state.state_name)
 		return
 
 	var new_state: State = get_state(new_state_name)
